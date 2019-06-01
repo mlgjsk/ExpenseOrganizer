aee1d8052abdfe2bee7509f683a71f4b2519fe4c
mlgjsk <buba_malogajski@outlook.com>
2019-06-01 11:19:24 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -3,28 +3,70 @@
 import { Dropbox } from 'dropbox';
 
 const dbx = new Dropbox({
-	accessToken: 'getyourownaccesstoken',
+	accessToken: 'get-your-own-access-token!',
 	fetch
 });
 
 const fileListElem = document.querySelector('.js-file-list');
+const loadingElem = document.querySelector('.js-loading');
+const rootPathForm = document.querySelector('.js-root-path__form');
+const rootPathInput = document.querySelector('.js-root-path__input');
+const organizeBtn = document.querySelector('.js-organize-btn');
+
+rootPathForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	state.rootPath = rootPathInput.value === '/' ? '' : rootPathInput.value.toLowerCase();
+	reset();
+});
+
+organizeBtn.addEventListener('click', async (e) => {
+	const originalMsg = e.target.innerHTML;
+	e.target.disabled = true;
+	e.target.innerHTML = 'Working...';
+	await moveFilesToDatedFolders();
+	e.target.disabled = false;
+	e.target.innerHTML = originalMsg;
+	reset();
+});
+
+const reset = () => {
+	state.files = [];
+	loadingElem.classList.remove('hidden');
+	init();
+};
 
 const state = {
-	files: []
+	files: [],
+	rootPath: ''
 };
 
 const init = async () => {
 	const res = await dbx.filesListFolder({
-		path: '',
+		path: state.rootPath,
 		limit: 20
 	});
 	updateFiles(res.entries);
+	if (res.has_more) {
+		loadingElem.classList.remove('hidden');
+		await getMoreFiles(res.cursor, (more) => updateFiles(more.entries));
+		loadingElem.classList.add('hidden');
+	} else {
+		loadingElem.classList.add('hidden');
+	}
 };
 
 const updateFiles = (files) => {
 	state.files = [ ...state.files, ...files ];
 	renderFiles();
 	getThumbnails(files);
+};
+
+const getMoreFiles = async (cursor, cb) => {
+	const res = await dbx.filesListFolderContinue({ cursor });
+	if (cb) cb(res);
+	if (res.has_more) {
+		await getMoreFiles(res.cursor, cb);
+	}
 };
 
 const renderFiles = () => {
@@ -72,6 +114,28 @@ const getThumbnails = async (files) => {
 	});
 	state.files = newStateFiles;
 	renderFiles();
+};
+
+const moveFilesToDatedFolders = async () => {
+	const entries = state.files.filter((file) => file['.tag'] === 'file').map((file) => {
+		const date = new Date(file.client_modified);
+		return {
+			from_path: file.path_lower,
+			to_path: `${state.rootPath}/${date.getFullYear()}/${date.getUTCMonth() + 1}/${file.name}`
+		};
+	});
+	try {
+		let res = await dbx.filesMoveBatchV2({ entries });
+		const { async_job_id } = res;
+		if (async_job_id) {
+			do {
+				res = await dbx.filesMoveBatchCheckV2({ async_job_id });
+				console.log(res);
+			} while (res['.tag'] === 'in_progress');
+		}
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 init();
